4e9855ea5bf20c5925473d973c33c5629e68412b
JubayerCodes <jubayerhossain111220@gmail.com>
2026-04-13 12:51:23 +0600

    added client domain in cors 2

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notfound from "./app/middlewares/notFound";
-import { envVars } from "./app/config/env";
 
 const app = express();
 
@@ -13,19 +12,6 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["https://star-gadgets-client.onrender.com"],
-    credentials: true,
-  }),
-);
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
     credentials: true,
   }),
 );
